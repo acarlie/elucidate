db770a07e869183bd8b55fcc655fdeed6170d4c9
acarlie <ameliacarlie@gmail.com>
2020-09-06 16:29:46 -0400

    feature(prettier husky): added and configured prettier and husky

--- src/stories/button.component.ts
+++ src/stories/button.component.ts
@@ -12,6 +12,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   </button>`,
   styleUrls: ['./button.css'],
 })
+
 export default class ButtonComponent {
   /**
    * Is this the principal call to action on the page?
